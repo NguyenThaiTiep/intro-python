a0f12d7bf17f6ec79555d7b94b6e0b2b73f0ab75
NguyenThaiTiep <nguyenthaitiep206@gmail.com>
2021-11-17 23:58:06 +0700

    intro data type

---
+++ list.py
@@ -0,0 +1,9 @@
+square = [1, 4, 9, 16, 25]
+
+print(square)
+print(square[-1:4])
+square[3] **= 2;
+print(square)
+print(square.count(4))
+q
+square.sort()
---
+++ string.py
@@ -0,0 +1,7 @@
+str = 'python'
+print(str)
+print(str[2])
+print(str[1:])
+print(str[:2])
+print(str[2 : 6])
+print(str[:])
